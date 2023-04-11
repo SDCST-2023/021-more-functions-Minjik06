5eca5a19a12fda88df0523ae9e0d2c90bf353e9b
Minjik06 <minjik525@deltalearns.ca>
2023-04-11 11:35:37 -0700

    4/11/2023

--- task3.py
+++ task3.py
@@ -14,12 +14,22 @@ Sample assertions:
 assert hypotenuse(6,8) == 10
 (2 points)
 """
+import math
 
-
-def hypotenuse():
-    pass
-
-assert hypotenuse(6,8) == 10
-assert hypotenuse(5,12) == 13
-assert hypotenuse(4,6) == 7.21
-assert hypotenuse(-3,4) == None
+def hypotenuse(a,b):
+    try:
+        a=float(a)
+        b=float(b)
+        c = math.sqrt(math.pow(a,2)+math.pow(b,2))
+        print(round(c, 2))
+        return round(c, 2)
+    except:
+        print("None")
+        return None
+try:
+    assert hypotenuse(6,8) == 10
+    assert hypotenuse(5,12) == 13
+    assert hypotenuse(4,6) == 7.21
+    assert hypotenuse(-3,4) == 5
+except:
+    print("Wrong")
